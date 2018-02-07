bfdfc8d00e790dd236dcce6d020a3dc7edc33037
ilevantis <ilevantis@users.noreply.github.com>
2018-02-07 02:22:03 +0000

    initial commit

---
+++ TiddlyWiki Export.js
@@ -0,0 +1,44 @@
+{
+	"translatorID": "7b9fc4f3-ef96-4bb2-a7e3-087da12f6ac4",
+	"label": "TiddlyWiki Export",
+	"creator": "IL",
+	"target": "json",
+	"minVersion": "3.0",
+	"maxVersion": "",
+	"priority": 100,
+	"displayOptions": {
+		"exportCharset": "UTF-8xBOM",
+		"exportNotes": false
+	},
+	"inRepository": false,
+	"translatorType": 2,
+	"lastUpdated": "2016-06-21 08:45:20"
+}
+
+
+
+function doExport() {
+	let item;
+	var jstring = 'javascript:(function(){$tw.wiki.addTiddlers([';
+	while (item = Zotero.nextItem()) {
+		let auths = [];
+		for (i = 0; i < item.creators.length; i++) {
+			auths.push((item.creators[i].firstName ? item.creators[i].firstName + ' ' : '') + item.creators[i].lastName);
+		}
+
+		let tiddler = {
+			papertitle : item.title,
+			title: item.citekey,
+			journal : item.publicationTitle,
+			doi : item.DOI,
+			year : ZU.strToDate(item.date).year,
+			abstract : item.abstractNote,
+			authors : auths.join(", "),
+			type : "text/paper",
+			tags : "paper"
+		};
+		jstring += 'new $tw.Tiddler($tw.wiki.getModificationFields(),' + JSON.stringify(tiddler) + '),'
+	}
+	jstring += '])})();'
+	Zotero.write(jstring);
+}
---
+++ TiddlyWiki JSON.js
@@ -0,0 +1,44 @@
+{
+	"translatorID": "faaac503-93a6-48ae-b18d-3cfba59b34ba",
+	"label": "TiddlyWiki JSON",
+	"creator": "IL",
+	"target": "json",
+	"minVersion": "3.0",
+	"maxVersion": "",
+	"priority": 100,
+	"displayOptions": {
+		"exportCharset": "UTF-8xBOM",
+		"exportNotes": false
+	},
+	"inRepository": false,
+	"translatorType": 2,
+	"lastUpdated": "2016-06-21 08:45:20"
+}
+
+
+
+function doExport() {
+	const tiddlers = [];
+	let item;
+	while (item = Zotero.nextItem()) {
+
+		let auths = [];
+		for (i = 0; i < item.creators.length; i++) {
+			auths.push((item.creators[i].firstName ? item.creators[i].firstName + ' ' : '') + item.creators[i].lastName);
+		}
+
+		let tiddler = {
+			papertitle : item.title,
+			title: item.citekey,
+			journal : item.publicationTitle,
+			doi : item.DOI,
+			year : ZU.strToDate(item.date).year,
+			abstract : item.abstractNote,
+			authors : auths.join(", "),
+			type : "text/paper",
+			tags : "paper"
+		};
+		tiddlers.push(tiddler)
+	}
+	Zotero.write(JSON.stringify(tiddlers));
+}
